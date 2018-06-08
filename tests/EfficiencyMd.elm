c00dc628e48b74a745f4b1f895b033743ec4b3f8
Marle <jakobrydhof@gmail.com>
2018-06-08 09:11:44 +0200

    remake effeciency test to be more precise

--- tests/EfficiencyMd.elm
+++ tests/EfficiencyMd.elm
@@ -1,37 +1,64 @@
-module ShowTests exposing (..)
+module Name exposing (Model, Msg, update, view, subscriptions, init)
 
-import LongMarkdown exposing (longMarkdown)
+import Html exposing (..)
 import Markdown
+import Time exposing (Time)
+import Task
+import LongMarkdown
 
 
---import Markdown
-
-import Fuzz exposing (..)
-import Test exposing (..)
-import Expect exposing (..)
-import Test.Runner.Html exposing (..)
-import Test.Html.Query as Query
-import Test.Html.Selector exposing (tag)
-import Result exposing (Result)
-
-
-main : TestProgram
+main : Program Never (Model msg) Msg
 main =
-    [ testLongString ]
-        |> concat
-        |> run
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-testLongString : Test
-testLongString =
-    describe "Test the time it takes to parse a long string"
-        [ test "Long Markdown String" <|
-            \_ ->
-                Expect.true "The Parsing succeeds" <|
-                    case Markdown.markdown longMarkdown of
-                        Result.Ok _ ->
-                            True
+type alias Model msg =
+    { time1 : Time
+    , time2 : Time
+    , md : Html msg
+    }
 
-                        Result.Err _ ->
-                            False
+
+type Msg
+    = StartTime Time
+    | EndTime Time
+
+
+update : Msg -> Model msg -> ( Model msg, Cmd Msg )
+update msg model =
+    case msg of
+        StartTime time ->
+            ( { model
+                | time1 = time
+                , md = Markdown.toHtml LongMarkdown.longMarkdown
+              }
+            , Task.perform EndTime Time.now
+            )
+
+        EndTime time ->
+            ( { model | time2 = time }, Cmd.none )
+
+
+view : Model msg -> Html Msg
+view model =
+    div []
+        [ p [] [ text "New Html Program" ]
+        , p [] [ text ("Start time: " ++ (toString model.time1)) ]
+        , p [] [ text ("End time: " ++ (toString model.time2)) ]
+        , p [] [ text ("Time taken: " ++ (toString (model.time2 - model.time1))) ]
         ]
+
+
+subscriptions : Model msg -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+init : ( Model msg, Cmd Msg )
+init =
+    ( Model 0 0 (text ""), Task.perform StartTime Time.now )
